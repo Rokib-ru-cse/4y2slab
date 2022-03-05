9b94a17704f030cbe99b412eb193465679a1f9b5
rokib-ru-cse <rokibahmed.ru.cse@gmail.com>
2022-03-06 01:46:54 +0600

    solved lehmann primality test

---
+++ crytography/LehmannPrimalityTest.java
@@ -0,0 +1,42 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class LehmannPrimalityTest {
+
+    private static int lehmann(BigInteger bigInteger, int tries) {
+        long a = (long) (Math.random() * (bigInteger.longValue() - 1)) + 1;
+        long e = bigInteger.subtract(BigInteger.valueOf(1)).divide(BigInteger.valueOf(2)).longValue() ;
+        while(tries>0){
+            int result = ((int)Math.pow(a, e))%bigInteger.intValue();
+            if((result%bigInteger.intValue())==1||(result%bigInteger.intValue()==(bigInteger.subtract(BigInteger.valueOf(1)).intValue()))){
+                 a = (long) (Math.random() * (bigInteger.longValue() - 1)) + 1;
+                tries--;
+            }else{
+                return -1;
+            }
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number to be tested : ");
+        BigInteger bigInteger = input.nextBigInteger();
+        int tries;
+        System.out.print("Enter the number of tries : ");
+        tries = input.nextInt();
+        if (bigInteger.equals(BigInteger.valueOf(2))) {
+            System.out.println("The number " + bigInteger + " is prime");
+        } else if (bigInteger.remainder(BigInteger.valueOf(2)) == BigInteger.valueOf(0)) {
+            System.out.println("The number " + bigInteger + " is composite");
+        } else {
+            long flag = lehmann(bigInteger, tries);
+            if (flag == 1) {
+                System.out.println(bigInteger + " may be Prime.");
+            } else {
+                System.out.println(bigInteger + " is Composite.");
+            }
+        }
+    }
+
+}
